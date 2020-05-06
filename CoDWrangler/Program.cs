df247ec439aa3a00690d83f8a84bc51a12587af8
Adam Sinclair <adam0sinclair@icloud.com>
2020-05-06 21:47:45 +0100

    Add project files.

---
+++ CoDWrangler/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace CoDWrangler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (; ; )
+            {
+                Process[] pname = Process.GetProcessesByName("ModernWarfare");
+                if (pname.Length == 0)
+                {
+                    Console.WriteLine("ModernWarfare not running");
+                }
+                else
+                {
+                    Console.WriteLine($"{pname.Count()} ModernWarfares running, checking priority class");
+                    foreach (Process process in Process.GetProcessesByName("ModernWarfare"))
+                    {
+                        Console.WriteLine($"ModernWarfare PID {process.Id} has priority class {process.PriorityClass.ToString()}");
+                        if (process.PriorityClass != ProcessPriorityClass.Normal)
+                        {
+                            Console.WriteLine($"Setting PID {process.Id} priority Normal");
+                            process.PriorityClass = ProcessPriorityClass.Normal;
+                            Console.WriteLine($"PID {process.Id} now has priority {process.PriorityClass.ToString()}");
+                        }
+                    }
+                }
+                System.Threading.Thread.Sleep(10000);
+            }
+        }
+    }
+}
