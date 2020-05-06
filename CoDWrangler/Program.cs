8475bd8436e66c3a8f2f7ea7105f73aeadada4b7
Adam Sinclair <adam0sinclair@icloud.com>
2020-05-07 00:50:32 +0100

    Add interval argument -t

--- CoDWrangler/Program.cs
+++ CoDWrangler/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace CoDWrangler
 {
@@ -8,6 +9,30 @@ namespace CoDWrangler
     {
         static void Main(string[] args)
         {
+            string argGrab = null;
+            Dictionary<string, string> argValues = new Dictionary<string, string>
+            {
+                { "-t", "10000" }
+            };
+            foreach (string arg in args)
+            {
+                if (null != argGrab)
+                {
+                    Console.WriteLine($"{argGrab} {arg}");
+                    argValues[argGrab] = arg;
+                    argGrab = null;
+                    continue;
+                }
+                if (argValues.Keys.Contains(arg))
+                {
+                    argGrab = arg;
+                    continue;
+                }
+            }
+            int interval = int.Parse(argValues["-t"]);
+
+            Console.WriteLine($"Interval: {interval.ToString()}");
+
             for (; ; )
             {
                 Process[] pname = Process.GetProcessesByName("ModernWarfare");
@@ -29,7 +54,7 @@ namespace CoDWrangler
                         }
                     }
                 }
-                System.Threading.Thread.Sleep(10000);
+                System.Threading.Thread.Sleep(interval);
             }
         }
     }
